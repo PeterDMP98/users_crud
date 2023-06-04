f7aeb8dded8ad97dbca24087ad3d842ce57dce88
PeterDMP98 <peter.dmp.ca@gmail.com>
2023-06-04 17:52:55 -0500

    advances

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userRouter = require('./user.Router');
+const userRouter = require('./user.router');
 const router = express.Router();
 
 // colocar las rutas aquí
